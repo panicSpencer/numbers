54a7e81d3c5d08a23622fb5b079d2bf129bce9c9
panicSpencer <72724866+panicSpencer@users.noreply.github.com>
2020-12-02 15:44:30 -0800

    Update numbers.sh

--- numbers.sh
+++ numbers.sh
@@ -4,15 +4,14 @@
 echo "Enter a positive integer"
 read i
 N=1
-while "$N" -le "$i"
-	do
-		if $((N%2)) -eq 0
-		then
-			echo $N "even"
-			N=$((N+1))
-		else
-			echo $N "odd"
-			N=$((N+1))
-		fi
+while ["$N" -le "$i"]
+	if $((N%2)) -eq 0
+	then
+		echo $N "even"
+		N=$((N+1))
+	else
+		echo $N "odd"
+		N=$((N+1))
+	fi
 done
 echo "Thank you"
