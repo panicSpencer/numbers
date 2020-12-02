1516a39eeac2322983ab66c775806bb08408d723
panicSpencer <72724866+panicSpencer@users.noreply.github.com>
2020-12-01 22:43:29 -0800

    Update numbers.sh

--- numbers.sh
+++ numbers.sh
@@ -2,11 +2,17 @@
 # numbers.sh
 # author Spencer Panic
 echo "Enter a positive integer"
-read integer
-while echo "$integer" | egrep -v "^[a-z][a-z,0-9,_]$" > /dev/null 2>&1
+read i
+N=1
+while "$N" -le "$i"
 do
-	echo "You must enter a valid username - no caps or special charachters!"
-	echo "Enter a username which only includes lower case letters, digits, and the underscore charachter: "
-	read username
+	if $((N%2)) -eq 0
+	then
+		echo $N "even"
+		N=$[$N+1]
+	else
+		echo $N "odd"
+		N=$[$N+1]
+	fi
 done
 echo "Thank you"
